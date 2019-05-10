d03d4edabb35c84b5385ba28bdf2d500021dcfd5
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-10 12:06:03 +0200

    Type basics and assigning types

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log("It works!");
+// string
+let myName = "Ola";
+//myName = 34; - error
+
+
+//number
+let myAge = 24;
+//myAge = "Ola"; - error
+
+
+// boolean
+let hasHobbies = true;
+//hasHobbies = 1; - error
+
+
+// assign types
+let myRealAge: number;
+//myRealAge = '26'; - error
+myRealAge = 24;
