cb4dbd2111cf86377f724e99acb90ed6ece934c4
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-10 18:28:29 +0200

    Type alias.

--- app.ts
+++ app.ts
@@ -11,3 +11,13 @@ let complex: {data: number[], output: (all: boolean) => number[] } = {
         return this.data;
     }
 };
+
+//type alias
+type Complex = {data: number[], output: (all: boolean) => number[] }
+
+let complex2: Complex = {
+    data: [20, 30, 40],
+    output: function (all: boolean): number[] {
+        return this.data;
+    }
+};
