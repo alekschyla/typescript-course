89bc9486f337092b21fb08dc79c47a42cda416eb
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-13 10:00:30 +0200

    Union types

--- app.ts
+++ app.ts
@@ -1,23 +1,5 @@
-//objects
-let userData: {name: string, age: number} = {
-    name: 'Ola',
-    age: 24,
-};
+// union types
+let myRealAge: number | string = 27;
+myRealAge = '27';
+//myRealAge = true; - error
 
-//complex object
-let complex: {data: number[], output: (all: boolean) => number[] } = {
-    data: [20, 30, 40],
-    output: function (all: boolean): number[] {
-        return this.data;
-    }
-};
-
-//type alias
-type Complex = {data: number[], output: (all: boolean) => number[] }
-
-let complex2: Complex = {
-    data: [20, 30, 40],
-    output: function (all: boolean): number[] {
-        return this.data;
-    }
-};
