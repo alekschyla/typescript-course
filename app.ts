0caf2507061ba502dbd2aa171abb078c82ad098a
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-13 12:04:04 +0200

    Nullable type

--- app.ts
+++ app.ts
@@ -9,7 +9,18 @@ if (typeof  finalValue == "number") {
     console.log('Final Value is a number');
 }
 
-// never type
+// never type - TypeScript 2.0
 function neverReturns(): never {
     throw new Error('An error');
 }
+
+// Nullable type - TypeScript 2.0
+let canBeNull: number | null = 12;
+canBeNull = null;
+
+let canAlsoBeNull;
+canAlsoBeNull = null;
+
+let canThisBeAny: number = null;
+canThisBeAny = 12;
+
