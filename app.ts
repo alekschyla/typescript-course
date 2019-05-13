64bd9d21c7e3b9654e1cce6d2c26a6eee3907b12
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-13 10:15:22 +0200

    Check types

--- app.ts
+++ app.ts
@@ -3,3 +3,8 @@ let myRealAge: number | string = 27;
 myRealAge = '27';
 //myRealAge = true; - error
 
+// check types
+let finalValue = 30;
+if (typeof  finalValue == "number") {
+    console.log('Final Value is a number');
+}
