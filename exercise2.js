29d80056bfa9b1824144909d2e40e4b9f827846e
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-15 12:52:08 +0200

    Solved exercise 2.

---
+++ exercise2.js
@@ -0,0 +1,57 @@
+// Exercise 1 - Maybe use an Arrow Function?
+var double = function(value) {
+    return value * 2;
+};
+console.log(double(10));
+
+const newDouble = value => value * 2;
+console.log(newDouble(10));
+
+
+// Exercise 2 - If only we could provide some default values...
+var greet = function (name) {
+    if (name === undefined) { name = "Max"; }
+    console.log("Hello, " + name);
+};
+greet();
+greet("Anna");
+
+const newGreet = (name = "Kate") => console.log(`Hello, ${name}`);
+newGreet();
+newGreet("Mary");
+
+// Exercise 3 - Isn't there a shorter way to get all these Values?
+var numbers = [-3, 33, 38, 5];
+console.log(Math.min.apply(Math, numbers));
+
+let newNumbers = [2, 4, 6, 8];
+console.log(Math.min(...newNumbers));
+
+// Exercise 4 - I have to think about Exercise 3 ...
+var newArray = [55, 20];
+Array.prototype.push.apply(newArray, numbers);
+console.log(newArray);
+
+let myArray = [13, 18];
+myArray.push(...newNumbers);
+console.log(myArray);
+
+// Exercise 5 - That's a well-constructed array.
+var testResults = [3.89, 2.99, 1.38];
+var result1 = testResults[0];
+var result2 = testResults[1];
+var result3 = testResults[2];
+console.log(result1, result2, result3);
+
+let [res1, res2, res3] = testResults;
+console.log(res1, res2, res3);
+
+// Exercise 6 - And a well-constructed object!
+var scientist = {firstName: "Will", experience: 12};
+var firstName = scientist.firstName;
+var experience = scientist.experience;
+console.log(firstName, experience);
+
+let person = {name: 'Kate', age: 23};
+let {name, age} = person;
+console.log(name, age);
