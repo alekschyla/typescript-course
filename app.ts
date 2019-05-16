4169b1340bf620a6189716a40a7fe56d9eef67a7
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-16 12:39:05 +0200

    Getters & Setters

--- app.ts
+++ app.ts
@@ -1,41 +1,25 @@
-class Person {
-    name: string;
-    private type: string;
-    protected age: number = 21;
+class Plant {
+    private _species: string = "Default";
 
-    constructor(name: string, public username: string) {
-        this.name = name;
+    get species () {
+        return this._species;
     }
 
-    printAge() {
-        console.log(this.age);
-        this.setType('Cool');
-    }
-
-    private setType (type: string) {
-        this.type = type;
-        console.log(this.type);
+    set species(value: string) {
+        if (value.length > 3) {
+            this._species = value;
+        } else {
+            this._species = "Default";
+        }
     }
 }
 
-const person = new Person('Kate', 'Katie1995');
-console.log(person);
-console.log(person.name, person.username);
-person.printAge();
-//person.setType('Cool');
-
-class Ola extends Person {
-    //name = 'Ola';
-
-    constructor(username: string) {
-        super("Ola", username);
-        this.age = 24;
-    }
-
-}
-
-const ola = new Ola("ola");
-console.log(ola);
+let plant = new Plant();
+console.log(plant.species);
+plant.species = "AB";
+console.log(plant.species);
+plant.species = "Rose";
+console.log(plant.species);
 
 
 
