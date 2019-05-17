3c504ed1c1b90b966ff83626f552b6d808c40a70
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-17 13:36:25 +0200

    Private Constructors

--- app.ts
+++ app.ts
@@ -1,39 +1,20 @@
-// Static properties and methods
-class Helpers {
-    static PI: number = 3.14;
-    static calcCircumference(diameter: number): number {
-        return this.PI * diameter;
+// Private Constructors
+class OnlyOne {
+    private static instance: OnlyOne;
+    private constructor (public name: string) {}
+
+    static getInstance() {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
     }
 }
 
-console.log(Helpers.PI);
-console.log(Helpers.calcCircumference(5));
 
-// Abstract Classes
-abstract class Project {
-    projectName: string = 'Default';
-    budget: number = 1000;
-
-    abstract changeName (name: string): void;
-
-    calcBudget () {
-        return this.budget * 2;
-    }
-}
-
-class ITProject extends Project {
-    changeName(name: string): void {
-        this.projectName = name;
-    }
-}
-
-let newProject = new ITProject();
-console.log(newProject);
-newProject.changeName('Super IT Project');
-console.log(newProject);
-
-
-
+let wrong = new OnlyOne;
+let right = OnlyOne.getInstance();
+console.log(wrong, right);
 
 
 
