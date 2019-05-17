87e33083f3fdad50435feba20b8133fec860f099
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-17 12:47:11 +0200

    Abstract Classes

--- app.ts
+++ app.ts
@@ -9,6 +9,41 @@ class Helpers {
 console.log(Helpers.PI);
 console.log(Helpers.calcCircumference(5));
 
+// Abstract Classes
+abstract class Project {
+    projectName: string = 'Default';
+    budget: number = 1000;
+
+    abstract changeName (name: string): void;
+
+    calcBudget () {
+        return this.budget * 2;
+    }
+}
+
+class ITProject extends Project {
+    changeName(name: string): void {
+        this.projectName = name;
+    }
+}
+
+let newProject = new ITProject();
+console.log(newProject);
+newProject.changeName('Super IT Project');
+console.log(newProject);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
