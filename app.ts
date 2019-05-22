8ea85e3d5c08c8ab8053dc8fadf5cc79e5e3ad06
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-22 12:20:06 +0200

    Interfaces with classes

--- app.ts
+++ app.ts
@@ -1,19 +1,19 @@
-interface NamesPerson {
+interface NamedPerson {
     name: string;
     age?: number;
     [propName: string]: any;
     greet(lastName: string): void;
 }
 
-function greet(person: NamesPerson): void {
+function greet(person: NamedPerson): void {
     console.log('Hello, ' + person.name);
 }
 
-function changeName(person: NamesPerson) {
+function changeName(person: NamedPerson) {
     person.name = "Anna";
 }
 
-const person = {
+const person1 = {
     name: 'Ola',
     hobbies: ['cooking', 'writing'],
     greet(lastName: string) {
@@ -22,6 +22,34 @@ const person = {
 };
 
 // greet({name: 'Mary', age: 22});
-changeName(person);
-greet(person);
-person.greet('Nowak');
+changeName(person1);
+greet(person1);
+person1.greet('Nowak');
+
+class PersonInterfaces implements NamedPerson {
+    name: string;
+    lastName: string;
+    greet(lastName: string) {
+        console.log('Hi, I am ' + this.name + ' ' + lastName);
+    }
+}
+
+const myPerson = new PersonInterfaces();
+myPerson.name = 'Kasia';
+myPerson.lastName = 'Kowalska';
+greet(myPerson);
+myPerson.greet(myPerson.lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
