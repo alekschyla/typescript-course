d2e44b7b73de3386d03c61f2b8ce247d2416936e
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-23 10:42:03 +0200

    Interface Inheritance

--- app.ts
+++ app.ts
@@ -8,3 +8,25 @@ const doubleFunction: DoubleValue = (value1: number, value2: number) => {
 };
 
 console.log(doubleFunction(3, 5));
+
+// Interface Inheritance
+interface NamedPerson {
+    name: string;
+    age?: number;
+    [propName: string]: any;
+    greet(lastName: string): void;
+}
+
+interface AgedPerson extends NamedPerson {
+    age: number;
+}
+
+const oldPerson: AgedPerson = {
+    age: 89,
+    name: 'Kate',
+    greet(lastName: string): void {
+        console.log('Hello!');
+    }
+};
+console.log(oldPerson);
+oldPerson.greet('Smith');
