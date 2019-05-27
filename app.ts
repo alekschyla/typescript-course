4d401e2a3d7cc60673130361f3758f9421b84a6e
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-27 14:41:41 +0200

    Method decorators

--- app.ts
+++ app.ts
@@ -1,43 +1,24 @@
-function logged(constructorFunction: Function) {
-    console.log(constructorFunction);
-}
-
-@logged
-class MyPerson {
-    constructor() {
-        console.log("Hi")
+function editable(value: boolean) {
+    return function (target: any, propName: string, descriptor: PropertyDescriptor) {
+        descriptor.writable = value;
     }
 }
 
-// Factory
-function logging(value: boolean) {
-    return value ? logged : null;
-}
+class Project {
+    projectName: string;
+    constructor(name: string) {
+        this.projectName = name;
+    }
 
-@logging(true)
-class MyCar {
-
-}
-
-// Advanced
-function printable(constructorFunction: Function) {
-    constructorFunction.prototype.print = function () {
-        console.log(this);
+    @editable(false)
+    calcBudget() {
+        console.log(1000);
     }
 }
 
-@logging(true)
-@printable
-class Plant {
-    name = "Green Plant"
-}
-
-const newPlant = new Plant();
-(<any>newPlant).print();
-
-
-
-
-
-
-
+const project = new Project('Super Project');
+project.calcBudget();
+project.calcBudget = function () {
+    console.log(2000);
+};
+project.calcBudget();
